57b9b714689f65f05b82ec6571ff0fbe438c51f6
kinkladze11 <118253730+kinkladze11@users.noreply.github.com>
2023-12-03 17:59:13 +0800

    Update Footer.js

--- components/molecules/Footer.js
+++ components/molecules/Footer.js
@@ -8,11 +8,6 @@ const Footer = () => {
         predictions, starting 11, player ratings, match stats for all football
         today. Covered 1000+ football results daily.
       </p>
-      <p className=" text-sm ">
-        Latest football scores. Our live scores provide football results with
-        predictions, starting 11, player ratings, match stats for all football
-        today. Covered 1000+ football results daily.
-      </p>
       <div className=" mt-2">
         <span className="">2022 Hivegoal - All Rights Reserved.</span>
         <span className="">Term of use, Privacy & Cookies policy Contact</span>
